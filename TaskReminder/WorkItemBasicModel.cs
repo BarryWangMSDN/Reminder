43a1b3f66e4f0720bf296ee110568ec7b30b0a21
Barry <wjp_barry@hotmail.com>
2018-05-17 18:04:03 +0800

    add item, not so sure, just update

---
+++ TaskReminder/WorkItemBasicModel.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskReminder
+{
+    class WorkItemBasicModel
+    {
+        private DateTime starttime;
+
+        public DateTime StartTime
+        {
+            get { return starttime; }
+            set { starttime = value; }
+        }
+
+        private DateTime duetime;
+
+        public DateTime DueTime
+        {
+            get { return duetime; }
+            set { duetime = value; }
+        }
+
+        private string itemurl;
+
+        public string ItemUrl
+        {
+            get { return itemurl; }
+            set { itemurl = value; }
+        }
+   
+        private List<string> workitempriority;
+
+        public List<string> WorkItemPriority
+        {
+            get { return workitempriority; }
+            set { workitempriority = value; }
+        }
+
+        
+
+
+    }
+}
