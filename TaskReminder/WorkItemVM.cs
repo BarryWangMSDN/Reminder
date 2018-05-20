af210c04791b9dd9cb29eefe1006dda984125b20
Barry Wang <wjp_barry@hotmail.com>
2018-05-20 21:54:54 +0800

    workitem change

---
+++ TaskReminder/WorkItemVM.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskReminder
+{
+    public class WorkItemVM
+    {
+        WorkItemVM()
+        {
+            ObservableCollection<WorkItemBasicModel> workitems = new ObservableCollection<WorkItemBasicModel>();
+
+        }
+
+        public void InsertWorkItemTest(string connectingString)
+        {
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectingString))
+                {
+                    
+
+                }
+            }
+            catch (Exception eSql)
+            {
+                Debug.WriteLine("Exception: " + eSql.Message);
+            }
+
+        }
+
+    }
+}
